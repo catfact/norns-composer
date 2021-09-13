d6e8f1dca4339716c7b5091e22f33f65cf44d672
emb <no@no>
2021-09-13 15:21:06 +0700

    add polyperc params

--- composer.lua
+++ composer.lua
@@ -65,9 +65,33 @@ init = function()
     
   clk = clock.run(clock_loop)
  
-  -- TODO:
-  -- start/stop/reset handlers
-  -- ...
+  ------------------------
+  --- polyperc params, stolen from `awake`
+  
+   cs_AMP = controlspec.new(0,1,'lin',0,0.5,'')
+  params:add{type="control",id="amp",controlspec=cs_AMP,
+    action=function(x) engine.amp(x) end}
+
+  cs_PW = controlspec.new(0,100,'lin',0,50,'%')
+  params:add{type="control",id="pw",controlspec=cs_PW,
+    action=function(x) engine.pw(x/100) end}
+
+  cs_REL = controlspec.new(0.1,3.2,'lin',0,1.2,'s')
+  params:add{type="control",id="release",controlspec=cs_REL,
+    action=function(x) engine.release(x) end}
+
+  cs_CUT = controlspec.new(50,5000,'exp',0,800,'hz')
+  params:add{type="control",id="cutoff",controlspec=cs_CUT,
+    action=function(x) engine.cutoff(x) end}
+
+  cs_GAIN = controlspec.new(0,4,'lin',0,1,'')
+  params:add{type="control",id="gain",controlspec=cs_GAIN,
+    action=function(x) engine.gain(x) end}
+  
+  cs_PAN = controlspec.new(-1,1, 'lin',0,0,'')
+  params:add{type="control",id="pan",controlspec=cs_PAN,
+    action=function(x) engine.pan(x) end}
+  ------------------------
   
   load_all_patterns()
   
